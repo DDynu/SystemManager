0839dc1f0c38b58604d7a71f12aad781202af37b
DDynu <khoadang826@gmail.com>
2026-01-28 20:45:20 +0700

    Add frontend

---
+++ frontend/src/components/SystemCard.tsx
@@ -0,0 +1,11 @@
+export default function SystemCard() {
+  return (
+    <article>
+      <h2>
+        System Name
+      </h2>
+      
+
+    </article>
+  )
+}
---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>
+        SYSTEM MANAGER
+      </h1>
+    </div>
+  )
+}
